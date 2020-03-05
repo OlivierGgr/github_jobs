64ebe99809d7d94cb12fa8920c6785d94627c28c
Olivier Ggr <grandgerard.olivier@gmail.com>
2020-03-05 17:24:16 +0100

    Updates

---
+++ precache-manifest.d6520abf253f3833304380a174885b2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99b7a0ed00a6574df223a979bb6b401f",
+    "url": "/github_jobs/index.html"
+  },
+  {
+    "revision": "a4cb84f2708f47242023",
+    "url": "/github_jobs/static/css/main.44b1f03e.chunk.css"
+  },
+  {
+    "revision": "aebc0c5f24a941688dd0",
+    "url": "/github_jobs/static/js/2.3542bbea.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/github_jobs/static/js/2.3542bbea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4cb84f2708f47242023",
+    "url": "/github_jobs/static/js/main.ef3b53ad.chunk.js"
+  },
+  {
+    "revision": "d37b1a722ed29855f8fc",
+    "url": "/github_jobs/static/js/runtime-main.edf5dc02.js"
+  }
+]);
